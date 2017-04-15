580cf58076fa05667868d6441cbb309cc11845d8
sofia <sathenikos@gmail.com>
2017-04-15 17:55:33 -0400

    created application class for fixer API use demo

---
+++ src/main/java/demo/fixer/FixerDemoApplication.java
@@ -0,0 +1,20 @@
+package demo.fixer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by sofia on 4/15/17.
+ */
+
+/**
+ * Main application for Fixer API use demo.
+ */
+@SpringBootApplication
+public class FixerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FixerDemoApplication.class, args);
+    }
+
+}
